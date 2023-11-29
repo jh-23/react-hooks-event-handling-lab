8c82338bf02ca55aaac37b8c70bf164b8c8602c8
John Heger <heger7@gmail.com>
2023-11-29 08:31:28 -0600

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Component Here!
+
+function handleFocus(event) {
+    console.log('Good!');
+}
+
+function handleBlur(event) {
+    console.log('Hey! Eyes on me!');
+}
+
+
+function EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
